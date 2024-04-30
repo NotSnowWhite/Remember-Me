9ccf243cda003cae1b47a2f5329f079f6b559d66
NotSnowWhite <gflatch@att.net>
2024-04-29 19:11:33 -0500

    removed 2 instances of dist from gitignore. added get requests and updated port on server.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 8080;
 
 // middleware to parse json
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/api/notes' (req, res) => {
+    
+})
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
+    console.log(`Listening on port http://localhost:${PORT}`)
 });
