efffa59ba46c0376de925a6ab15a527a5a7c5c4b
NotSnowWhite <gflatch@att.net>
2024-04-29 20:34:34 -0500

    added get and post for db data. created get request to read db data and return that data to the file. created post request to read db data and return it to the body

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const PORT = 8080;
 // middleware to parse json
 app.use(express.json());
 
+// middleware to serve static files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
@@ -19,9 +20,39 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-app.get('/api/notes' (req, res) => {
-    
-})
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        const notes = JSON.parse(data);
+        res.json(notes)
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    fs.readFile('db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        const notes = JSON.parse(data);
+
+        newNote.id = notes.length + 1;
+        notes.push(newNote);
+        console.log(newNote.id);
+
+        fs.writeFile('db.json', JSON.stringify(notes, null, 4), (err) => {
+            if (err) {
+                console.error(err);
+            }
+
+            res.json(newNote);
+        });
+    })
+});
+
+// listening for requests 
 app.listen(PORT, () => {
     console.log(`Listening on port http://localhost:${PORT}`)
 });
